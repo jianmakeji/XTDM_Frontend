3f657af9a05235a58c632da1d91be4f854784025
codeliling <maximuslee@126.com>
2017-07-11 13:30:37 +0800

    fix bugs

--- XTDM/js/project/contentManage.js
+++ XTDM/js/project/contentManage.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	
+	$("#addContentSign").html('');
 	$("#categoryId").hide();
 	
 	$("#addContent").click(function(){
 		let categoryId = $("#categoryId").html();
 		$("#managePanel").empty();
 		$("#managePanel").load("addContent.html");
+		$("#addContentSign").html("1");
 	});
 });
--- XTDM/js/project/header.js
+++ XTDM/js/project/header.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	$("#addContentSign").hide();
 	$("#managePanel").empty();
 	$("#managePanel").load("contentManage.html");
 });
@@ -6,21 +7,48 @@ $(document).ready(function() {
 var app = new Vue({
 	el: '#dropdown1',
 	data: {
-		menus:[
-			{'id':'1','name':'人文'},{'id':'2','name':'物语'},{'id':'3','name':'风景'},{'id':'4','name':'社区'},{'id':'5','name':'音乐'}
-		]
+		menus: [{
+			'id': '1',
+			'name': '人文'
+		}, {
+			'id': '2',
+			'name': '物语'
+		}, {
+			'id': '3',
+			'name': '风景'
+		}, {
+			'id': '4',
+			'name': '社区'
+		}, {
+			'id': '5',
+			'name': '音乐'
+		}]
 	},
 	methods: {
-		menuClick: function(event,index) {
+		menuClick: function(event, index) {
 			let clickObj = $('#dropdown1 div').eq(index);
 			let text = clickObj.text();
 			let id = clickObj.attr('id');
 			//let breadcrumbs = document.getElementById("breadcrumbs");
 			//breadcrumbs.innerText = "内容管理   > " + text;
-			
-			
-			$("#breadcrumbs").html("内容管理   > " + text);
-			$("#categoryId").html(id);
+			alert($("#addContentSign").html());
+			if($("#addContentSign").html() == 1) {
+				$.ajax({
+					type: "get",
+					url: "contentManage.html",
+					async: false,
+					success: function(data) {
+						$("#managePanel").html(data);
+						$("#breadcrumbs").html("内容管理   > " + text);
+						$("#categoryId").html(id);
+					}
+				});
+
+			} else {
+				$("#breadcrumbs").html("内容管理   > " + text);
+				$("#categoryId").html(id);
+			}
+
 		}
 	}
 })
