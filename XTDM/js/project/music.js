b3176495d6aab300c457a0dd516c51e9b00c4730
liling <maximuslee@126.com>
2017-07-16 13:28:06 +0800

    调整css js代码结构

---
+++ XTDM/js/project/music.js
@@ -0,0 +1,46 @@
+var vum = new Vue({
+	el: '#dataTable',
+	data: {
+		datas: "",
+		pageNum: "",
+		activeNumber: 1,
+		pageCount: 10
+	},
+	methods: {
+		showData: function() {
+			$.getJSON("music.json", function(data) {
+				vum.datas = data;
+			});
+		}
+	}
+});
+
+(function() {
+
+	var currentPage = 0;
+
+	$.getJSON("music.json", function(data) {
+		var pageNum = data.length / 10;
+		if(data.length % 10 != 0) {
+			pageNum = pageNum + 1;
+		}
+		vum.datas = data.slice(currentPage * 10, currentPage * 10 + 10);
+		vum.pageNum = pageNum;
+	});
+
+	var showCategory = 0;
+	$("#addMusic").click(function() {
+		if(showCategory == 0) {
+			showCategory = 1;
+			$("#addPanel").show('slow');
+		} else {
+			showCategory = 0;
+			$("#addPanel").hide();
+		}
+	});
+
+	$("#cancel").click(function() {
+		showCategory = 0;
+		$("#addPanel").hide();
+	});
+})();
